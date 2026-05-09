ccb69ff961f97a63c38e0890d2c5111365e03b40
rickyars <rickyars@gmail.com>
2026-05-09 16:31:14 -0500

    fix: rename display name to Tiled Image Detailer for discoverability

--- node_detailer.py
+++ node_detailer.py
@@ -97,5 +97,5 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LLMTileSequentialDetailer": "LLM Tile Sequential Detailer",
+    "LLMTileSequentialDetailer": "Tiled Image Detailer",
 }
